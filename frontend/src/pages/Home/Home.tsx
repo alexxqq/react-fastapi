1670523af0b16b785ff64dbc720aa05d962355cc
alex.ubuntu <thealex403@gmail.com>
2024-02-29 23:55:36 +0200

    add pipeline

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -11,7 +11,6 @@ export const Home = () => {
     return (
         <div className='track-hui'>
             <Nav shouldRender={shouldRender}></Nav>
-            <h1> no!! WORKS!!!!!</h1>
             <Tasks shouldRender={shouldRender}></Tasks>
         </div>
     )
