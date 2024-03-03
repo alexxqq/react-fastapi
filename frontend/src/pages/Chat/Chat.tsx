844605b7c52147b5dca38aa422be3716d64b43f3
alex <sasha79shapoval@gmail.com>
2024-03-03 23:46:07 +0200

    chat debug

--- frontend/src/pages/Chat/Chat.tsx
+++ frontend/src/pages/Chat/Chat.tsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef, useLayoutEffect } from 'react'
 import { Nav } from '../../components/Nav/Nav'
 import './chat.scss'
 import useRenderVerification from '../../Hooks/useVerification'
 import { Loading } from '../Loading/Loading'
+import { ChatEndpoint } from '../../common/constants/chat.endpoint'
 import chatService from '../../services/chat.service'
-import { ChatEndpoint } from '../../common/constants/chat.endpoint' 
 
 interface Message {
     message: string
@@ -14,25 +14,35 @@ export const Chat = () => {
     const shouldRender: any = useRenderVerification()
     const [messages, setMessages] = useState<Message[]>([])
     const [message, setMessage] = useState<string>()
-    console.log(shouldRender)
+    const [ws, setWs] = useState<WebSocket | null>(null)
+    const chatContainerRef = useRef<HTMLUListElement>(null);
 
-    const ws = new WebSocket(`${ChatEndpoint.WebSocket}/${shouldRender?.email}`)
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setMessage(event.target.value)
     }
 
     const handleMessage = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault()
-        if (message) {
+        if (message && ws) {
             ws.send(message)
         }
     }
+
+    useEffect(() => {
+        if (shouldRender) {
+            const wsInstance = new WebSocket(`${ChatEndpoint.WebSocket}/${shouldRender?.email}`)
+            setWs(wsInstance)
+        }
+        fnccc()
+    }, [shouldRender])
+    useLayoutEffect(() => {
+        scrollToBottom();
+      }, [messages]);
     const fnccc = () => {
         const fetchData = async () => {
             try {
                 const response:any = await chatService.getLastMessages()
                 const data = await response
-                console.log('setMessages! :', data)
                 setMessages(data)
             } catch (error) {
                 console.error('Error fetching data:', error)
@@ -43,15 +53,19 @@ export const Chat = () => {
 
         const handleMessageReceived = (event: any) => {
             const newMessage = { message: event.data }
+            console.log(newMessage)
             setMessages((prevMessages) => [...prevMessages, newMessage])
         }
 
-        ws.onmessage = handleMessageReceived
+        if (ws) {
+            ws.onmessage = handleMessageReceived
+        }
     }
-
-    useEffect(() => {
-        fnccc()
-    }, [])
+    const scrollToBottom = () => {
+        if (chatContainerRef.current) {
+          chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+        }
+      };
     if (shouldRender === null) {
         return <Loading />
     }
@@ -59,12 +73,11 @@ export const Chat = () => {
         window.location.href = 'error404'
         return null
     }
-
     return (
         <div className='track-hui'>
             <Nav shouldRender={shouldRender}></Nav>
             <div className='chat'>
-                <ul id='messages' className='mt-4'>
+                <ul id='messages' className='mt-4' ref={chatContainerRef}>
                     {messages.map((message) => (
                         <li key={message.message}>{message.message}</li>
                     ))}
