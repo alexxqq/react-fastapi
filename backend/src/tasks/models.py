4ac03f94a976c08205e4daa04100bd3f5ae40144
alex <sasha79shapoval@gmail.com>
2024-03-05 17:40:12 +0200

    security upd

--- backend/src/tasks/models.py
+++ backend/src/tasks/models.py
@@ -50,7 +50,7 @@ class Tag(Base):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
 
 
-DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 engine = create_engine(DATABASE_URL)
 
 # Bind the engine to the metadata
